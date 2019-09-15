c09a8faba4f2972e653287dae08d6c1f03293abc
yihang <1341674619@qq.com>
2019-09-15 18:28:05 +0800

    master

--- app/middleware/gzip.js
+++ app/middleware/gzip.js
@@ -1,42 +1,12 @@
 /*
  * @Author: tao 
  * @Date: 2019-06-28 16:58:39 
- * @Last Modified by: tao
- * @Last Modified time: 2019-06-28 16:59:12
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-09-15 18:24:23
  * @func 对数据进行gzip压缩
  */
-const isJSON = require('koa-is-json');
-const zlib = require('zlib');
-const Stream = require('stream');
-function readStream(readStream){
-
-  return new Promise( reslove =>{
-    let html = '';
-    readStream.on('data',chuck=>{
-      html += chuck;
-    });
-    readStream.on('end',()=>{
-      reslove(html);
-    });
-  });
-
-}
+const options = { threshold: 1024 };
+const compress = require('koa-compress');
 module.exports = () => {
-  return async function gzip(ctx, next) {
-    await next();
-    
-    // 后续中间件执行完成后将响应体转换成 gzip
-    let body = ctx.body;
-    if (!body) return;
-    if (isJSON(body)){
-      body = JSON.stringify(body);
-    }else if(body instanceof Stream){
-      body = await readStream(body);
-    }
-    // 设置 gzip body，修正响应头
-    const stream = zlib.createGzip();
-    stream.end(body);
-    ctx.body = stream;
-    ctx.set('Content-Encoding', 'gzip');
-  };
+  return compress(options);
 };
--- config/plugin.js
+++ config/plugin.js
@@ -3,10 +3,10 @@
 /** @type Egg.EggPlugin */
 module.exports = {
   // 配置模板引擎 egg-view-nunjucks
-  nunjucks: {
-    enable: true,
-    package: 'egg-view-nunjucks',
-  },
+  // nunjucks: {
+  //   enable: true,
+  //   package: 'egg-view-nunjucks',
+  // },
   // 引入参数校验插件
   validate: {
     enable: true,
